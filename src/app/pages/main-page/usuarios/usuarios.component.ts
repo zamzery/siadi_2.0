2c6c95da1c24e0738e75da658cbeb35c69db4645
Eduardo Martínez <zamzery@gmail.com>
2022-03-23 14:02:57 -0600

    Componente de usuarios

--- src/app/pages/main-page/usuarios/usuarios.component.ts
+++ src/app/pages/main-page/usuarios/usuarios.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Iusuarios } from "src/app/interface/iusuarios";
 import { UsuariosService } from "src/app/services/usuarios.service";
+import { MatTableDataSource } from "@angular/material/table";
 
 @Component({
 	selector: "app-usuarios",
@@ -12,14 +13,39 @@ export class UsuariosComponent implements OnInit {
 
 	constructor(private usuariosService: UsuariosService) {}
 
+	displayedColumns: string[] = [
+		"nombre",
+		"telefono",
+		"cargo",
+		"imagen",
+		"statusUsuario",
+		"Acciones",
+	];
+	dataSource!: MatTableDataSource<Iusuarios>;
+
 	ngOnInit() {
 		this.getData();
 		console.log(this.users);
 	}
 
 	getData() {
-		this.usuariosService.getData().subscribe((resp) => {
-			console.log("resp:", resp);
+		this.usuariosService.getData().subscribe((resp: any) => {
+			this.users = Object.keys(resp).map(
+				(a) =>
+					({
+						id: a,
+						nombre: resp[a].nombre,
+						telefono: resp[a].telefono,
+						email: resp[a].email,
+						cargo: resp[a].cargo,
+						username: resp[a].username,
+						imagen: resp[a].imagen,
+						statusUsuario: resp[a].statusUsuario,
+						permisos: resp[a].permisos,
+					} as Iusuarios)
+			);
+			this.dataSource = new MatTableDataSource(this.users);
+			console.log("Data-Source: ", this.dataSource);
 		});
 	}
 }
--- src/app/pages/main-page/usuarios/usuarios.module.ts
+++ src/app/pages/main-page/usuarios/usuarios.module.ts
@@ -1,17 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { UsuariosComponent } from './usuarios.component';
-import { UsuariosRoutes } from './usuarios.routing';
-
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatTableModule } from "@angular/material/table";
+import { UsuariosComponent } from "./usuarios.component";
+import { UsuariosRoutes } from "./usuarios.routing";
 
 @NgModule({
-    declarations: [UsuariosComponent],
-    imports: [
-        CommonModule,
-        UsuariosRoutes
-    ]
+	declarations: [UsuariosComponent],
+	imports: [CommonModule, UsuariosRoutes, MatTableModule],
 })
-export class UsuariosModule { }
+export class UsuariosModule {}
